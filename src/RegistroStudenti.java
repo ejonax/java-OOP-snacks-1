462551429f9a8961a8b5794b2c902d6f11601c8a
ejonax <ejonax@yahoo.it>
2025-02-03 15:42:03 +0100

    playing with Studente constructor

--- src/RegistroStudenti.java
+++ src/RegistroStudenti.java
@@ -4,16 +4,17 @@ public class RegistroStudenti {
        
         Studente[] studens= new Studente[5];
         
-        studens[0]=new Studente("ejona1","xheka1");
-        studens[1]=new Studente("ejona2","xheka2");
-        studens[2]=new Studente("ejona3","xheka3");
-        studens[3]=new Studente("ejona4","xheka4");
-        studens[4]=new Studente("ejona5","xheka5");
+        studens[0]=new Studente("ejona1","xheka1",41);
+        studens[1]=new Studente("ejona2","xheka2",42);
+        studens[2]=new Studente("ejona3","xheka3",43);
+        studens[3]=new Studente("ejona4","xheka4",44);
+        studens[4]=new Studente("ejona5","xheka5",45);
         System.out.println(studens[0].getNome());
     }
 } 
 
-    class Studente {
+   /* 
+   class Studente {
         String nome;
         String cognome;
     
@@ -39,3 +40,4 @@ public class RegistroStudenti {
         }
     }
 
+*/
